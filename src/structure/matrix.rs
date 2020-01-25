9bcb5bc64cf08a51c43b989eedb5434282b35bb4
Vui-Chee <vuicheesiew@gmail.com>
2020-01-25 16:47:09 +0800

    Location logic shifted to matrix.rs
    Process of selecting random neighbours to traverse
    is moved into matrix module.
    
    Now dfs will have cleaner logic.

--- src/structure/matrix.rs
+++ src/structure/matrix.rs
@@ -1,7 +1,12 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+use super::types::*;
+
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
-    markers: Vec<bool>, // Store as one dimensional array
+    pub markers: Vec<bool>, // Store as one dimensional array
 }
 
 impl Matrix {
@@ -31,9 +36,37 @@ impl Matrix {
         let idx = self.index(i, j);
         self.markers[idx] = val;
     }
+
+    /// Returns a randomized vec of possible locations surrounding
+    /// cell.
+    pub fn get_neighbours(&self, i: usize, j: usize) -> Vec<Location> {
+        let mut locations = vec![];
+        if i >= 1 {
+            locations.push((i - 1, j));
+        }
+        if i + 1 < self.rows {
+            locations.push((i + 1, j));
+        }
+        if j >= 1 {
+            locations.push((i, j - 1));
+        }
+        if j + 1 < self.cols {
+            locations.push((i, j + 1));
+        }
+
+        // Shuffle slice
+        let mut rng = thread_rng();
+        locations.as_mut_slice().shuffle(&mut rng);
+
+        locations
+    }
 }
 
 #[cfg(test)]
 mod matrix_tests {
+    // use super::*;
     //
+    // fn get_correct_neighbours() {
+    // //
+    // }
 }
