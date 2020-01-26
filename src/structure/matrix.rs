22488c3de71e3025699dbb9c3123bd0a685635d5
Vui-Chee <vuicheesiew@gmail.com>
2020-01-26 22:02:37 +0800

    Some tests for matrix structure.

--- src/structure/matrix.rs
+++ src/structure/matrix.rs
@@ -64,9 +64,81 @@ impl Matrix {
 
 #[cfg(test)]
 mod matrix_tests {
-    // use super::*;
-    //
-    // fn get_correct_neighbours() {
-    // //
-    // }
+    use super::*;
+
+    #[test]
+    fn get_correct_index() {
+        let mat = Matrix::new(5, 5);
+        assert_eq!(mat.index(0, 0), 0);
+        assert_eq!(mat.index(4, 4), 24);
+        assert_eq!(mat.index(2, 2), 12);
+        assert_eq!(mat.index(3, 4), 19);
+        assert_eq!(mat.index(1, 2), 7);
+    }
+
+    #[test]
+    fn put_correct_value() {
+        let mut mat = Matrix::new(5, 5);
+        mat.put(1, 3, true);
+        assert!(mat.at(1, 3));
+        mat.put(2, 4, true);
+        assert!(mat.at(2, 4));
+        mat.put(0, 3, true);
+        assert!(mat.at(0, 3));
+
+        // Check remaining markers are still false.
+        let mut index = 0;
+        for marker in mat.markers.iter() {
+            if index != (1 * 5 + 3) && index != (2 * 5 + 4) && index != (0 * 5 + 3) {
+                assert!(!marker);
+            }
+            index += 1;
+        }
+    }
+
+    #[test]
+    fn get_correct_neighbours() {
+        let mat = Matrix::new(5, 5);
+
+        // CORNERS
+        // top left corner
+        let mut locations = mat.get_neighbours(0, 0);
+        locations.sort();
+        assert_eq!(locations, vec![(0, 1), (1, 0)]);
+        // top right corner
+        let mut locations = mat.get_neighbours(0, 4);
+        locations.sort();
+        assert_eq!(locations, vec![(0, 3), (1, 4)]);
+        // bottom left corner
+        let mut locations = mat.get_neighbours(4, 0);
+        locations.sort();
+        assert_eq!(locations, vec![(3, 0), (4, 1)]);
+        // bottom right corner
+        let mut locations = mat.get_neighbours(4, 4);
+        locations.sort();
+        assert_eq!(locations, vec![(3, 4), (4, 3)]);
+
+        // EDGES
+        // top edge
+        let mut locations = mat.get_neighbours(0, 2);
+        locations.sort();
+        assert_eq!(locations, vec![(0, 1), (0, 3), (1, 2)]);
+        // left edge
+        let mut locations = mat.get_neighbours(2, 0);
+        locations.sort();
+        assert_eq!(locations, vec![(1, 0), (2, 1), (3, 0)]);
+        // right edge
+        let mut locations = mat.get_neighbours(2, 4);
+        locations.sort();
+        assert_eq!(locations, vec![(1, 4), (2, 3), (3, 4)]);
+        // bottom edge
+        let mut locations = mat.get_neighbours(4, 2);
+        locations.sort();
+        assert_eq!(locations, vec![(3, 2), (4, 1), (4, 3)]);
+
+        // center
+        let mut locations = mat.get_neighbours(2, 2);
+        locations.sort();
+        assert_eq!(locations, vec![(1, 2), (2, 1), (2, 3), (3, 2)]);
+    }
 }
